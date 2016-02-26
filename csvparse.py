108659c1d184653db4c802183e1bc2bdf1f7dc2e
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2016-02-26 03:24:35 -0500

    Added average vision difficulty

--- csvparse.py
+++ csvparse.py
@@ -8,6 +8,9 @@ cognitive_na = 0
 ambu_na=0 #Variable declaration for Ambu
 ambu_no=0
 ambu_yes=0
+count_1 = 0
+count_2 = 0
+other_count = 0
 for row in mycsv:
 	if isFirstrow:
 		isFirstrow = False
@@ -18,9 +21,9 @@ for row in mycsv:
 	if drem_value == '1':
 		cognitive_yes += 1
 	elif drem_value == '2':
-		cognitive _no += 1
+		cognitive_no += 1
 	else:	
-		congnitive_na += 1
+		cognitive_na += 1
 	#ambu
 	ambu_value = row[249] 
 	if ambu_value=='1':  #Yes for Ambu
@@ -29,17 +32,26 @@ for row in mycsv:
         	ambu_no+=1
     	else: #No for Ambu
         	ambu_na+=1
+    #Vison effect Deye
+    #Deye for vision
+	deye_count = row[247]
+	if deye_count == '1':
+		count_1 += 1
+	elif deye_count == '2':
+		count_2 += 1
+	else:
+		other_count += 1
 #total_population is the same for all difficulties
-total_population += cognitive_yes + cognitive_no + congnitive_na
+total_population += cognitive_yes + cognitive_no + cognitive_na
 #average cognitive difficulty
-avg_cogn_yes = (cognitive_yes / total_population) * 10
-avg_cogn_no = (cognitive_no / total_population) * 100
-avg_cogn_na = (cognitive_ / total_population) * 10
+avg_cogn_yes = (float(cognitive_yes) / total_population) * 100
+avg_cogn_no = (float(cognitive_no) / total_population) * 100
+avg_cogn_na = (float(cognitive_na) / total_population) * 100
 #average ambulatory difficulty
-avg_ambu_yes = (ambu_yes / total_population) * 100 
-avg_ambu_no = (ambu_no / total_population) * 100
-avg_ambu_na = (ambu_na / total_population) * 100
-
-
-
-
+avg_ambu_yes = (float(ambu_yes) / total_population) * 100 
+avg_ambu_no = (float(ambu_no) / total_population) * 100
+avg_ambu_na = (float(ambu_na) / total_population) * 100
+#average vision difficulty
+vision_difficulty_yes = (float(count_1) / total_population) * 100
+vision_difficulty_no = (float(count_2) / total_population) * 100
+vision_na = (float(other_count) / total_population) * 100
