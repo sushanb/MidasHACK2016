c9d420cb71a1a4e93b1fc1aa7139d3fdd9e2f996
Sumit Dhungel <dhungelsumit2014@gmail.com>
2016-02-26 03:34:40 -0500

    added hearing difficulty average

--- csvparse.py
+++ csvparse.py
@@ -8,9 +8,12 @@ cognitive_na = 0
 ambu_na=0 #Variable declaration for Ambu
 ambu_no=0
 ambu_yes=0
-count_1 = 0
+count_1 = 0#variable declaration for vison difficulty 
 count_2 = 0
 other_count = 0
+hearing_yes = 0 #variable declaration for hearing disability
+hearing_no = 0
+hearing_na = 0
 for row in mycsv:
 	if isFirstrow:
 		isFirstrow = False
@@ -41,6 +44,15 @@ for row in mycsv:
 		count_2 += 1
 	else:
 		other_count += 1
+	#Hearing difficulty
+	#dear
+	dear_value=row[246]
+	if dear_value == '1':
+		hearing_yes += 1
+	elif dear_value == '2':
+		hearing_no += 1
+	else:	
+		hearing_na += 1
 #total_population is the same for all difficulties
 total_population += cognitive_yes + cognitive_no + cognitive_na
 #average cognitive difficulty
@@ -55,3 +67,8 @@ avg_ambu_na = (float(ambu_na) / total_population) * 100
 vision_difficulty_yes = (float(count_1) / total_population) * 100
 vision_difficulty_no = (float(count_2) / total_population) * 100
 vision_na = (float(other_count) / total_population) * 100
+#average hearing difficulty
+avg_hear_yes = (float(hearing_yes)/total_population)*100
+avg_hear_no = (float(hearing_no)/total_population)*100
+avg_hear_na = (float(hearing_na)/total_population)*100
+
